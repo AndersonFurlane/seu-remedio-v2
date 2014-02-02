ccbb969b089ae5f44e2d0e695d6b30cee88cf6e8
Anderson Furlane <andersonfurlanemartins@gmail.com>
2014-02-02 01:33:11 -0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeuRemedioV2::Application.config.secret_key_base = '20b39025418e6cc5de94d9154fbf41e4261f51109bf7e129bfd3379a219923b95e865b6c1cd06f58e5e877242b214330835cd77ed25ca532df3cb7090263336c'
